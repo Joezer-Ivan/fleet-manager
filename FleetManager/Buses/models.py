b7246cbc2e1fcd92bfcf52423ff92a8c14105c97
Joezer-Ivan <joezerivan@gmail.com>
2016-10-13 19:22:29 +0530

    updates models

--- FleetManager/Buses/models.py
+++ FleetManager/Buses/models.py
@@ -8,3 +8,40 @@ class Routes(models.Model):
 
 	def __str__(self):
 		return self.stage+self.route
+
+
+class Stop(models.Model):
+	STOP_TYPES = (
+		('A', 'All'),
+		('O', 'Ordinary'),
+	)
+	name = models.CharField(max_length = 30)
+	buses_that_stop_here = models.CharField(max_length = 1, choices = STOP_TYPES)
+
+	def __str__(self):
+		return self.name
+
+
+class Route(models.Model):
+	number = models.CharField(max_length = 10, primary_key = True)
+	stops = models.ManyToManyField(Stop)
+
+	def __str__(self):
+		return self.number
+
+
+class Bus(models.Model):
+	BUS_TYPES = (
+		('AC', 'Air Conditioned'),
+		('DE', 'Deluxe'),
+		('EX', 'Express'),
+		('OR', 'Ordinary'),
+		('SM', 'Small'),
+	)
+
+	license_plate = models.CharField(max_length = 10, primary_key = True)
+	route = models.ForeignKey('Route', on_delete = PROTECT)
+	bus_type = models.CharField(max_length = 2, choices = BUS_TYPES)
+
+	def __str__(self):
+		return self.license_plate
