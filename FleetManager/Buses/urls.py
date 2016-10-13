595f031be4e4c1a8c8b2f2a1d41ebd7c7ce2abdd
Joezer-Ivan <joezerivan@gmail.com>
2016-10-13 19:32:10 +0530

    minor changes

--- FleetManager/Buses/urls.py
+++ FleetManager/Buses/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.areas_of_chennai,  name = 'areas_of_chennai'),
-    url(r'^mtc$', views.mtc,  name = 'mtc'),
-    url(r'^routes$', views.Bus_route,  name = 'Bus_route'),
+    url(r'^mtc/$', views.mtc,  name = 'mtc'),
+    url(r'^routes/$', views.Bus_route,  name = 'Bus_route'),
 
 ]
